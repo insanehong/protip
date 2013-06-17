8751c9a10ad4c8d58f7451f73a6ee607b6426103
Daegeun Kim <dgkim84@gmail.com>
2013-06-18 01:31:40 +0900

    ProTip 초기 버전 개발
    * 기본적인 내부 함수 구현 완료
    * data-path / data-auto / data-interval / data-shuffle / data-rate
    * $(selector).protip();

--- protip.jquery.js
+++ protip.jquery.js
@@ -22,41 +22,69 @@
     constructor : ProTip,
 
     initialize : function() {
-      this.$element.css({'z-index' : '999'});
+      var self = this;
+      var enable = this.options.rate >= Math.random();
+      if (enable) {
+        $.get(this.options.path, function(json) {
+          self.cursor = 0;
+          self.tips = self.options.shuffle === false ? json :
+              json.sort(function() { return 0.5 - Math.random(); });
+          self.show();
+          if (self.options.auto === true) {
+            self.timer = setInterval(self.next.bind(self), self.options.interval);
+            self.$element.on('mouseover', function() {
+              self.pause();
+            })
+            .on('mouseout', function() {
+              self.resume();
+            });
+          }
+        });
+      }
     },
-    // start rollting
-    start : function() {
-
-    },
-    // stop rolling
-    stop : function() {
-
-    },
-    // next protip
-    next : function() {
-
-    },
-    // remove protip
-    close : function() {
-
-    },
-    // show protip
     show : function() {
-
+      this.stopped = false;
+      this.$element.html(this.tips[this.cursor]);
+      this.$element.show();
+    },
+    resume : function() {
+      this.stopped = false;
+    },
+    pause : function() {
+      this.stopped = true;
+    },
+    next : function() {
+      if (this.started === true) {
+        this.cursor++;
+        this.cursor = this.cursor % this.tips.length;
+      }
+    },
+    close : function() {
+      clearInterval(this.timer);
+      this.$element.remove();
     }
   };
 
   $.fn.protip = function (option) {
     return this.each(function () {
-      var $this = $(this),
-        data = $this.data('protip'),
-        options = $.extend({}, $.fn.protip.defaults, typeof option == 'object' && option);
+      var $this = $(this);
+      option = option || {};
+      option.path = option.path || $this.data('path');
+      option.auto = option.auto || $this.data('auto');
+      option.rate = option.rate || $this.data('rate');
+      option.shuffle = option.shuffle || $this.data('shuffle');
+      option.interval = option.interval || $this.data('interval');
+
+      var data = $this.data('protip'),
+        options = $.extend({}, $.fn.protip.defaults, option);
 
       if (!data) {
         $this.data('protip', (data = new ProTip(this, options)));
         data.initialize();
+        $this.find(options.btnClose).on('click', function() {
+          data.close();
+        });
       }
-      if (option == 'show') data.show();
     });
   };
 
@@ -64,7 +92,8 @@
     interval : 60000,
     auto : false,
     path: './protip.json',
-    rate : 0.3,
+    rate : 1,
+    shuffle : true,
     btnClose : '.btn-protip-close'
   };
 
